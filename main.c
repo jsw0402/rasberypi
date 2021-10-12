0a9cf7d8f5f0a11c8d46b6cc24e88e4fdf35d346
issiscv <issiscv46@gmail.com>
2021-10-12 18:32:43 +0900

    test

--- main.c
+++ main.c
@@ -1,71 +1,100 @@
-#include <stdio.h> 
-#include <string.h> 
-#include <errno.h> 
-#include <wiringPi.h> 
-#include <wiringPiSPI.h> 
 
-#define CS_MCP3208 11 //GPIO 8 
-#define SPI_CHANNEL 0 
-#define SPI_SPEED 1000000 //1Mhz
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-// spi communication with Rpi and get sensor data 
+#define MAX_TIMINGS    85
+#define DHT_PIN        11    /* GPIO-22 */
 
-int read_mcp3208_adc(unsigned char adcChannel)
+int data[5] = { 0, 0, 0, 0, 0 };
+
+void read_dht_data()
 {
-	unsigned char buff[3];
-	int adcValue = 0;
+	uint8_t laststate = HIGH;
+	uint8_t counter = 0;
+	uint8_t j = 0, i;
 
-	buff[0] = 0x06 | ((adcChannel & 0x07) >> 2);
-	buff[1] = ((adcChannel & 0x07) << 6);
-	buff[2] = 0x00;
+	data[0] = data[1] = data[2] = data[3] = data[4] = 0;
 
-	digitalWrite(CS_MCP3208, 0);
-	wiringPiSPIDataRW(SPI_CHANNEL, buff, 3);
+	/* pull pin down for 18 milliseconds */
+	pinMode(DHT_PIN, OUTPUT);
+	digitalWrite(DHT_PIN, LOW);
+	delay(18);
 
-	buff[1] = 0x0f & buff[1];
-	adcValue = (buff[1] << 8) | buff[2];
+	/* prepare to read the pin */
+	pinMode(DHT_PIN, INPUT);
 
-	digitalWrite(CS_MCP3208, 1);
+	/* detect change and read data */
+	for (i = 0; i < MAX_TIMINGS; i++)
+	{
+		counter = 0;
+		while (digitalRead(DHT_PIN) == laststate)
+		{
+			counter++;
+			delayMicroseconds(1);
+			if (counter == 255)
+			{
+				break;
+			}
+		}
+		laststate = digitalRead(DHT_PIN);
 
-	return adcValue;
+		if (counter == 255)
+			break;
+
+		/* ignore first 3 transitions */
+		if ((i >= 4) && (i % 2 == 0))
+		{
+			/* shove each bit into the storage bytes */
+			data[j / 8] <<= 1;
+			if (counter > 16)
+				data[j / 8] |= 1;
+			j++;
+		}
+	}
+
+	/*
+	* check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
+	* print it out if data is good
+	*/
+	if ((j >= 40) &&
+		(data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)))
+	{
+		float h = (float)((data[0] << 8) + data[1]) / 10;
+		if (h > 100)
+		{
+			h = data[0];    // for DHT11
+		}
+		float c = (float)(((data[2] & 0x7F) << 8) + data[3]) / 10;
+		if (c > 125)
+		{
+			c = data[2];    // for DHT11
+		}
+		if (data[2] & 0x80)
+		{
+			c = -c;
+		}
+		float f = c * 1.8f + 32;
+		printf("Humidity = %.1f %% Temperature = %.1f *C (%.1f *F)\n", h, c, f);
+	}
+	else {
+		printf("Data not good, skip\n");
+	}
 }
 
-int main(void) {
+int main(void)
+{
+	printf("Raspberry Pi DHT11/DHT22 temperature/humidity test\n");
 
-	unsigned char adcChannel_light = 0;
-
-	int adcValue_light = 0;
-
-	float vout_light;
-	float vout_oftemp;
-	float percentrh = 0;
-	float supsiondo = 0;
-
-	printf("start");
-
-
-	if (wiringPiSetupGpio() == -1)
-	{
-		fprintf(stdout, "Unable to start wiringPi :%s\n", strerror(errno));
-		return 1;
-	}
-
-	if (wiringPiSPISetup(SPI_CHANNEL, SPI_SPEED) == -1)
-	{
-		fprintf(stdout, "wiringPiSPISetup Failed :%s\n", strerror(errno));
-		return 1;
-	}
-
-	pinMode(CS_MCP3208, OUTPUT);
+	if (wiringPiSetup() == -1)
+		exit(1);
 
 	while (1)
 	{
-		adcValue_light = read_mcp3208_adc(adcChannel_light);
-
-		//printf("Humiity = %u temparature = %u\n", adcValue_humi, adcValue_temp);
-		printf("light sensor = %u\n", adcValue_light);
-
-		delay(100);
+		read_dht_data();
+		delay(2000); /* wait 2 seconds before next read */
 	}
-	return 0;
+
+	return(0);
 }
