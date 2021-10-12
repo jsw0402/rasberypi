3c19ebb0ad833280da6f86fabaf5b8bcf92e8e52
issiscv <issiscv46@gmail.com>
2021-10-12 19:53:15 +0900

    test3

--- main.c
+++ main.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <stdlib.h>
 #include <wiringPi.h>
-#define CDS 10
+#define TP 20
+#define EP 21
+float getDistance(void)
+{
+	float fDistance;
+	int nStartTime, nEndTime;
+	digitalWrite(TP, LOW);
+	delayMicroseconds(2);
+	// pull the Trig pin to high level for more than 10us impulse
+	digitalWrite(TP, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(TP, LOW);
+	while (digitalRead(EP) == LOW);
+	nStartTime = micros();
+	while (digitalRead(EP) == HIGH);
+	nEndTime = micros();
+	fDistance = (nEndTime - nStartTime) / 29. / 2.;
+	return fDistance;
+}
 int main(void)
 {
-	if (wiringPiSetupGpio() == -1)
+	if (wiringPiSetupGpio() == -1) {
 		return 1;
-
-	pinMode(CDS, INPUT);
+	}
+	pinMode(TP, OUTPUT);
+	pinMode(EP, INPUT);
 	while (1)
 	{
-		if (digitalRead(CDS) == 0)
-			printf("Dark....\n");
-		if (digitalRead(CDS) == 1)
-			printf("Bright!!!\n");
-		delay(200);
+		float fDistance = getDistance();
+		printf("Distance:%.2fcm\n", fDistance);
+		delay(1000);
 	}
 	return 0;
-}
+}
