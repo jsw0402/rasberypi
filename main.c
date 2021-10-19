0226aeb303871912d6ee91da56f8992e78cde3d2
issiscv <issiscv46@gmail.com>
2021-10-19 18:07:10 +0900

    doremi

--- main.c
+++ main.c
@@ -1,36 +1,55 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <wiringPi.h>
-#define TP 20
-#define EP 21
-float getDistance(void)
+#define SPEAKER 1 // wPi GPIO 1
+#define BASIC_TIME 187 // 박자 기본 시간 16분음표의 출력 시간으로 지정
+void tone(int scale, int timing);
+int main()
 {
-	float fDistance;
-	int nStartTime, nEndTime;
-	digitalWrite(TP, LOW);
-	delayMicroseconds(2);
-	// pull the Trig pin to high level for more than 10us impulse
-	digitalWrite(TP, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(TP, LOW);
-	while (digitalRead(EP) == LOW);
-	nStartTime = micros();
-	while (digitalRead(EP) == HIGH);
-	nEndTime = micros();
-	fDistance = (nEndTime - nStartTime) / 29. / 2.;
-	return fDistance;
-}
-int main(void)
-{
-	if (wiringPiSetupGpio() == -1) {
+	char tone_scale[] = { 'g', 'e', 'e', 'g', 'e', 'c', 'd', 'e',
+		'd', 'c', 'e', 'g', 'C', 'g', 'C', 'g',
+		'C', 'g', 'e', 'g', 'd', 'f', 'e', 'd', 'c' }; // 계이름
+	int time_scale[] = { 4, 2, 2, 2, 2, 4, 4, 2, 2, 2, 2, 4, 3, 1,
+		2, 2, 2, 2, 4, 4, 2, 2, 2, 2, 4 };
+	// 4 : ♩ 3:♪ 2: ♪ 1: ♬	int counter = 0;
+	if (wiringPiSetup() == -1)
 		return 1;
-	}
-	pinMode(TP, OUTPUT);
-	pinMode(EP, INPUT);
-	while (1)
+	softToneCreate(SPEAKER); // 스피커 소리를 출력할 핀 번호 설정
+	for (counter = 0; counter < 25; counter++)
 	{
-		float fDistance = getDistance();
-		printf("Distance:%.2fcm\n", fDistance);
-		delay(1000);
+		tone(tone_scale[counter], time_scale[counter]); // 함수로 해당 음과 박자
+														//길이를 전달
 	}
-	return 0;
 }
+void tone(int scale, int timing){
+	int scale_sound; // 주파수 저장할 변수
+	switch (scale) // 음에 해당하는 주파수를 찾는다.
+	{
+	case 'c':
+		scale_sound = 261;
+		break;
+	case 'd':
+		scale_sound = 293;
+		break;	case 'e':
+		scale_sound = 329;
+		break;
+	case 'f':
+		scale_sound = 349;
+		break;
+	case 'g':
+		scale_sound = 391;
+		break;
+	case 'a':
+		scale_sound = 440;
+		break;
+	case 'b':
+		scale_sound = 493;
+		break;
+	case 'C':
+		scale_sound = 523;
+		break;
+	default:
+		scale_sound = 0;
+		break;
+	}
+	}
