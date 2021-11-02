5176541d27043f71477e12cad904985edd331037
issiscv <issiscv46@gmail.com>
2021-11-02 18:52:24 +0900

    sub

--- main.c
+++ main.c
@@ -1,42 +1,57 @@
-#include <errno.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
-#include <mosquitto.h>
+#include <signal.h>
 #include <stdio.h>
-#define MQTT_HOSTNAME "localhost"
-#define MQTT_PORT 1883
-#define MQTT_USERNAME "admin"
-#define MQTT_PASSWORD "admin"
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <mosquitto.h>
+#define mqtt_host "localhost"
+#define mqtt_port 1883
 #define MQTT_TOPIC "hello/world"
-int main(int argc, char **argv) {
-	struct mosquitto *mosq = NULL;
-	// 초기화
+static int run = 1;
+void handle_signal(int s)
+{
+	run = 0;
+}
+void connect_callback(struct mosquitto *mosq, void *obj, int result)
+{
+	printf("connect callback, rc=%d\n", result);
+}
+void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
+{
+	bool match = 0;
+	printf("got message '%.*s' for topic '%s'\n", message->payloadlen, (char*)message->payload, message->topic);
+	//printf("receive message(%s) : %s",message->topic, message->payload);
+	//mosquitto_topic_matches_sub("/devices/wb-adc/controls/+", message->topic, &match);
+	if (match) {
+		printf("got message for ADC topic\n");
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	uint8_t reconnect = true;
+	//char clientid[24];//id를 사용하는 경우
+	struct mosquitto *mosq;
+	int rc = 0;
+	signal(SIGINT, handle_signal);
+	signal(SIGTERM, handle_signal);
 	mosquitto_lib_init();
-	// 모스키토 런타임 객체와 클라이언트 랜덤 ID 생성
-	mosq = mosquitto_new(NULL, true, NULL);
-	if (!mosq) {
-		printf("Cant initiallize mosquitto library\n");
-		exit(-1);
+	mosq = mosquitto_new(NULL, true, 0);//mosquitto 구조체 생성
+	if (mosq) {
+		mosquitto_connect_callback_set(mosq, connect_callback);
+		mosquitto_message_callback_set(mosq, message_callback);
+		rc = mosquitto_connect(mosq, mqtt_host, mqtt_port, 60);//mosqutiio 서버와 연결
+		mosquitto_subscribe(mosq, NULL, MQTT_TOPIC, 0);//subscribe
+		while (run) {
+			rc = mosquitto_loop(mosq, -1, 1);
+			if (run && rc) {
+				printf("connection error!\n");
+				sleep(10);
+				mosquitto_reconnect(mosq);
+			}
+		}
+		mosquitto_destroy(mosq);
 	}
-	21 Publisher 코드
-		mosquitto_username_pw_set(mosq, MQTT_USERNAME, MQTT_PASSWORD);
-	// MQTT 서버 연결 설립, keep-alive 메시지 사용 안함
-	int ret = mosquitto_connect(mosq, MQTT_HOSTNAME, MQTT_PORT, 10);
-	if (ret) {
-		printf("Cant connect to mosquitto server\n");
-		exit(-1);
-	}
-	char text[20] = “I’m CppCode";
-		ret = mosquitto_publish(mosq, NULL, MQTT_TOPIC, strlen(text), text, 0, false);
-	if (ret) {
-		printf("Cant connect to mosquitto server\n");
-		exit(-1);
-	}
-	// 네트워크 동작이 끝나기 전에 모스키토 동작을 막기위해 잠깐의 딜레이가 필요함
-	//sleep(1);
-	mosquitto_disconnect(mosq);
-	mosquitto_destroy(mosq);
 	mosquitto_lib_cleanup();
-	return 0;
+	return rc;
 }
