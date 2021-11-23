eb447b4191f9781b12cb80cfc971f4d976581fa5
issiscv <issiscv46@gmail.com>
2021-11-23 18:44:25 +0900

    dong jak

--- main.c
+++ main.c
@@ -1,84 +1,28 @@
-#include <signal.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include <unistd.h>
-#include <mosquitto.h>
 #include <wiringPi.h>
-#define mqtt_host "localhost"
-#define mqtt_port 1883
-#define MQTT_TOPIC "house/myroom/lamp"
-#define OFF 0
-#define ON 1
-#define LED 4
-static int run = 1;
-int LedFlag = false;
-int LedData = OFF;
-const char* MegON = "ON";
-const char* MegOFF = "OFF";
-void handle_signal(int s)
+#define INPUT_PIN 4
+int g_nPirState = LOW;
+int g_nVal = 0;
+int main(void)
 {
-	run = 0;
-}
-void connect_callback(struct mosquitto *mosq, void *obj, int result)
-{
-	printf("connect callback, rc=%d\n", result);
-}
-void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
-{
-	printf("got message '%.*s' for topic '%s'\n", message->payloadlen, (char*)message->payload, message->topic);
-	if (strcmp(MegON, (char*)message->payload) == 0)
+	if (wiringPiSetupGpio() == -1)
+		return 1;
+	pinMode(INPUT_PIN, INPUT);
+	while (1)
 	{
-		LedFlag = true;
-		LedData = ON;
-		printf("LED ON\n");
+		g_nVal = digitalRead(INPUT_PIN);
+		if (g_nVal == HIGH)
+		{
+			if (g_nPirState == LOW)
+				printf("Motion Detected!!\n");
+			g_nPirState = HIGH;
+		}
+		else
+		{
+			if (g_nPirState == HIGH)
+				printf("Motion ended.\n");
+			g_nPirState = LOW;
+		}
 	}
-	else if (strcmp(MegOFF, (char*)message->payload) == 0)
-	{
-		LedFlag = true;
-		LedData = OFF;
-		printf("LED OFF\n");
-	}
-	else
-	{
-		printf("LED not\n");
-	}
-}
-
-int main(int argc, char *argv[])
-{
-	uint8_t reconnect = true;
-	//char clientid[24];//id를 사용하는 경우
-	struct mosquitto *mosq;
-	int rc = 0;
-	signal(SIGINT, handle_signal);
-	signal(SIGTERM, handle_signal);
-	mosquitto_lib_init();
-	wiringPiSetup();
-	pinMode(LED, OUTPUT);
-	mosq = mosquitto_new(NULL, true, 0);//mosquitto 구조체 생성
-	if (mosq) {
-		mosquitto_connect_callback_set(mosq, connect_callback);
-		mosquitto_message_callback_set(mosq, message_callback);
-		rc = mosquitto_connect(mosq, mqtt_host, mqtt_port, 60);//mosqutiio 서버와 연결
-		mosquitto_subscribe(mosq, NULL, MQTT_TOPIC, 0);//subscribe
-		
-			while (run)
-			{
-				rc = mosquitto_loop(mosq, -1, 1);
-				if (run && rc) {
-					printf("connection error!\n");
-					sleep(10);
-					mosquitto_reconnect(mosq);
-				}
-				if (LedFlag == true)
-				{
-					digitalWrite(LED, LedData);
-					LedFlag = false;
-				}
-			}
-		mosquitto_destroy(mosq);
-	}
-	mosquitto_lib_cleanup();
-	return rc;
+	return 0;
 }
