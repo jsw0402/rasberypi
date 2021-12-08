36872005a977c7853b79645803c149d4352f2e36
issiscv <issiscv46@gmail.com>
2021-12-08 14:03:01 +0900

    재업로드

--- CCPS.c
+++ CCPS.c
@@ -110,17 +110,16 @@ int main() {
 	}
 	else if (pid == 0) {
 		//자식 프로세스
-		//thread1 2 3 을 시작
 		run_thread();
 
 	}
 	else {
 		// 부모 프로세스
-		signal(SIGALRM, stopThread);//프로세스에 무어잇인 발생했음을 알리는 메시지
-		pthread_mutex_init(&cursorMutex, NULL);//뮤텍스(mutex)는 쓰레드가 공유하는 데이터 영역을 보호하기 위해서 사용되는 도구이다. pthread_mutex_init는 뮤텍스 객체를 초기화 시키기 위해서 사용한다.
-												//상호배제"가 되겠네요. 해석의 의미는 모호하지만 뮤텍스 객체를 두 쓰레드가 동시에 사용할 수 없다는 것입니다.
-		initPanel();//패널 시작
-		simulate();//0또는 1을 입력하여 시작
+		signal(SIGALRM, stopThread);
+		pthread_mutex_init(&cursorMutex, NULL);
+
+		initPanel();
+		simulate();
 	}
 	return 0;
 }
@@ -154,54 +153,53 @@ void drawInfo() {
 
 	//속도 부분 초기화
 	for (i = 10; i>0; i--) {
-		move_cur(SPEED_X - 4, i + 5);//15 - 4, i + 5
+		move_cur(SPEED_X - 4, i + 5);
 		if (i % 2 != 0) printf("%3d", (NAME_SPACE - 3 - i) * 20);
 	}
-	move_cur(SPEED_X - 2, NAME_SPACE);//15 - 2, 16
+	move_cur(SPEED_X - 2, NAME_SPACE);
 	printf("%5s", "SPEED");
-	move_cur(SPEED_X - 2, VALUE_SPACE);//15 - 2, 18
+	move_cur(SPEED_X - 2, VALUE_SPACE);
 	printf("%3d", 0);
 
 	//RPM부분 초기화
 	for (i = 12; i>0; i--) {
-		move_cur(RPM_X - 4, i + 3);// 30 - 4, i + 3
-		if (i % 2) printf("%3d", (NAME_SPACE - 3 - i) / 2);//16 - 3 - i / 2
+		move_cur(RPM_X - 4, i + 3);
+		if (i % 2) printf("%3d", (NAME_SPACE - 3 - i) / 2);
 	}
-	move_cur(RPM_X - 2, NAME_SPACE);//30 - 2, 16
+	move_cur(RPM_X - 2, NAME_SPACE);
 	printf("%5s", "RPM");
-	move_cur(RPM_X - 2, VALUE_SPACE);//30 - 2, 18
+	move_cur(RPM_X - 2, VALUE_SPACE);
 	printf("%3d", 0);
 
 
 	//연료 부분 초기화
-	move_cur(FUEL_X - 4, NAME_SPACE - 10);// 45 - 4, 16 - 10
+	move_cur(FUEL_X - 4, NAME_SPACE - 10);
 	printf("%3c", 'F');
-	move_cur(FUEL_X - 4, NAME_SPACE - 1);// 45 - 4, 16 - 1
+	move_cur(FUEL_X - 4, NAME_SPACE - 1);
 	printf("%3c", 'E');
-	move_cur(FUEL_X - 2, NAME_SPACE);// 45 - 2, 16
+	move_cur(FUEL_X - 2, NAME_SPACE);
 	printf("%5s", "FUEL");
-	move_cur(FUEL_X - 2, VALUE_SPACE);// 45 - 2, 18
+	move_cur(FUEL_X - 2, VALUE_SPACE);
 	printf("%3d", 0);
 
 	//냉각수 부분 초기화
-	move_cur(COOLANT_X - 2, NAME_SPACE);// 60 - 2, 16
+	move_cur(COOLANT_X - 2, NAME_SPACE);
 	printf("%5s", "COOLANT");
-	move_cur(COOLANT_X - 2, VALUE_SPACE);// 60 - 2, 18
+	move_cur(COOLANT_X - 2, VALUE_SPACE);
 	printf("%3d", 0);
 }
 
 //Panel 부분을 초기화 해주는 함수
 void initPanel() {
-	drawBoard();//초기 화면을 지우고 인터페이스의 틀을 그려주는 함수
-	drawInfo();//Panel 에서 보여줘야할 그래프를 초기화 해주는 함수 === 내부에 정보를 그려줌
+	drawBoard();
+	drawInfo();
 }
 
 //시뮬레이팅을 위한 함수
-//1 입력 시 simulMenu 시작
 void simulate() {
 	int input = -1;
 
-	move_cur(0, SIMUL_SPACE);//0, 22
+	move_cur(0, SIMUL_SPACE);
 	printf("What do you want? (0 : exit, 1 : simulation) >>");
 	__fpurge(stdin); //리눅스에서 __fpurge(stdin)
 	scanf("%d", &input);
@@ -221,7 +219,6 @@ void simulate() {
 }
 
 //시뮬레이션 메뉴 함수
-//속도를 입력함
 void simulMenu() {
 	int input = -1;
 	int p1Speed = 0;
@@ -238,7 +235,7 @@ void simulMenu() {
 		printf("                                                         ");
 		move_cur(0, SIMUL_SPACE);
 		printf("Please input Speed (20~160) >>");
-		__fpurge(stdin); //리눅스에서 __fpurge(stdin) 입력 버퍼 지우기
+		__fpurge(stdin); //리눅스에서 __fpurge(stdin)
 		scanf("%d", &p1Speed);
 		move_cur(0, SIMUL_SPACE);
 		printf("                                                         ");
@@ -247,7 +244,7 @@ void simulMenu() {
 			printf("                        ");
 			move_cur(0, SIMUL_SPACE);
 			printf("Please input Acceleration (1~10) >>");
-			__fpurge(stdin); //리눅스에서 __fpurge(stdin) 입력 버퍼 지우기
+			__fpurge(stdin); //리눅스에서 __fpurge(stdin)
 			scanf("%d", &p1Acc);
 			if (p1Acc >= 1 && p1Acc <= 10) {
 				//이부분에 프로세스간 통신 작성하면 됨
@@ -287,8 +284,8 @@ void *updateSpeed(void* arg) {
 	//close(pipe2[1]);
 	while (1) {
 		if(sync_th!=0) continue;
-		fcntl(pipe2[0], F_SETFL, O_NONBLOCK);//fcntl함수는 이미 열린 파일의 속성들을 변경할 수 있습니다.
-											//파일 상태 속성들을 세번째 인수로 받아 설정합니다. O_APPEND, O_NONBLOCK, O_SYNC, O_DSYNC, O_RSYNC, O_FSYNC, O_ASYNC 속성만 변경할 수 있습니다. 읽기, 쓰기 관련 플래그(O_WRONLY, O_RDONLY, O_RDWR)은 조회할 수는 있지만 변경할 수 없음을 유의하세요.
+		fcntl(pipe2[0], F_SETFL, O_NONBLOCK);
+
 
 		switch (nread = read(pipe2[0], &buffer2, sizeof(int))) {
 		case -1:
@@ -300,19 +297,18 @@ void *updateSpeed(void* arg) {
 			break;
 		}
 
-		//이 두 함수는 mutex를 이용하여 임계 구역을 진입할때 그 코드 구역을 잠그고 다시 임계 구역이 끝날때 다시 풀어 다음 스레드가 진입할 수 있도록 합니다.
 		pthread_mutex_lock(&cursorMutex);
 
 		speedChart = (int)((double)speed / 30);
 		for (i = 0; i<10; i++) {
-			move_cur(SPEED_X, CHART_SPACE - i);//15, 15 - i
+			move_cur(SPEED_X, CHART_SPACE - i);
 			printf("  ");
 		}
 		for (i = 0; i<speedChart; i++) {
-			move_cur(SPEED_X, CHART_SPACE - i);//15, 15 - i
+			move_cur(SPEED_X, CHART_SPACE - i);
 			printf("O");
 		}
-		move_cur(SPEED_X - 2, VALUE_SPACE);//15 - 2, 18
+		move_cur(SPEED_X - 2, VALUE_SPACE);
 		printf("%5d", speed);
 
 		sync_th++;
@@ -344,14 +340,14 @@ void *updateRPM(void* arg) {
 
 		rpmChart = (int)((double)rpm / 500);
 		for (i = 0; i<12; i++) {
-			move_cur(RPM_X, CHART_SPACE - i);//30, 15 - i
+			move_cur(RPM_X, CHART_SPACE - i);
 			printf("  ");
 		}
 		for (i = 0; i<rpmChart; i++) {
-			move_cur(RPM_X, CHART_SPACE - i);//30, 15 - i
+			move_cur(RPM_X, CHART_SPACE - i);
 			printf("O");
 		}
-		move_cur(RPM_X - 2, VALUE_SPACE);//30 - 2, 18
+		move_cur(RPM_X - 2, VALUE_SPACE);
 		printf("%5d", rpm);
 		sync_th++;
 		pthread_mutex_unlock(&cursorMutex);
@@ -383,14 +379,14 @@ void *updateFuel(void* arg) {
 
 		fuelChart = (int)((double)fuel / 10);
 		for (i = 0; i<10; i++) {
-			move_cur(FUEL_X, CHART_SPACE - i);//45, 15 - i
+			move_cur(FUEL_X, CHART_SPACE - i);
 			printf("  ");
 		}
 		for (i = 0; i<fuelChart; i++) {
-			move_cur(FUEL_X, CHART_SPACE - i);//45, 15 - i
+			move_cur(FUEL_X, CHART_SPACE - i);
 			printf("O");
 		}
-		move_cur(FUEL_X - 2, VALUE_SPACE);//45 - 2, 18
+		move_cur(FUEL_X - 2, VALUE_SPACE);
 		printf("%5d", fuel);
 		sync_th++;
 		pthread_mutex_unlock(&cursorMutex);
@@ -422,14 +418,14 @@ void *updateCoolant(void* arg) {
 
 		coolantChart = (int)((double)coolant / 10);
 		for (i = 0; i<10; i++) {
-			move_cur(COOLANT_X, CHART_SPACE - i);//60, 15 - i
+			move_cur(COOLANT_X, CHART_SPACE - i);
 			printf("  ");
 		}
 		for (i = 0; i<coolantChart; i++) {
-			move_cur(COOLANT_X, CHART_SPACE - i);//60, 15 - i 
+			move_cur(COOLANT_X, CHART_SPACE - i);
 			printf("O");
 		}
-		move_cur(COOLANT_X - 2, VALUE_SPACE);// 60 - 2, 18
+		move_cur(COOLANT_X - 2, VALUE_SPACE);
 		printf("%5d", coolant);
 		sync_th=0;
 		pthread_mutex_unlock(&cursorMutex);
@@ -441,7 +437,7 @@ void stopThread() {
 	pthread_mutex_lock(&cursorMutex);
 	sleep(1);
 
-	move_cur(0, SIMUL_SPACE+1);//0, 22 + 1
+	move_cur(0, SIMUL_SPACE+1);
 	exit(0);
 
 }
@@ -504,8 +500,7 @@ void *Figures_RPM(void* arg) {
 
 		//pthread_mutex_lock(&mut);
 		pthread_mutex_lock(&rpmut);
-		RPM = Speed * 1000000
-			/ Min / Tire_Perimeter_Ratio; // 60 / 1850
+		RPM = Speed * 1000000 / Min / Tire_Perimeter_Ratio;
 		write(pipe3[1], &RPM, sizeof(int));
 		//  pthread_cond_wait(&cmd, &mut);
 		pthread_mutex_unlock(&rpmut);
@@ -573,12 +568,12 @@ void *Figures_Coolant(void* arg) {
 void run_thread()
 {
 	int re = 0;
-	pthread_mutex_init(&mut, NULL);//첫번째 인자인 mutex는 초기화 시킬 mutex객체이다. 초기화 시킬 때 뮤텍스의 특징을 정의할 수 있는데, 이는 두번째 인자인 attr를 통해서 이루어진다. 기본 뮤텍스 특징을 사용하길 원한다면 NULL을 이용하면 된다.
+	pthread_mutex_init(&mut, NULL);
 	pthread_mutex_init(&rpmut, NULL);
-	pthread_cond_init(&cmd, NULL);//조건 변수를 초기화합니다. 이 함수말고 정적으로 조건 변수를 초기화할 경우에는 PTHREAD_CONT_INITIALIZER 상수를 이용해서 초기화할 수도 있습니다.
+	pthread_cond_init(&cmd, NULL);
 	pthread_t thread1,thread2,thread3,thread4;
 
-	re = pthread_create(&thread1, NULL, Figures_Speed, NULL);//쓰레드 생성 기본
+	re = pthread_create(&thread1, NULL, Figures_Speed, NULL);
 	if (re < 0) {
 		printf("threads[0] Figures_Speed error\n");
 		exit(0);
@@ -599,8 +594,8 @@ void run_thread()
 		exit(0);
 	}
 	sleep(5000);
-	pthread_mutex_destroy(&mut);//만약 뮤텍스를 동적으로 생성(pthread_mutex_init을 이용하여 초기화)했다면 이 함수를 사용하는 함수가 pthread_mutex_destroy입니다.
-	pthread_cond_destroy(&cmd);//pthread_cond_destroy() 함수는, 조건 변수 cond 에 의해 할당할 수 있었던 리소스를 해방합니다.
+	pthread_mutex_destroy(&mut);
+	pthread_cond_destroy(&cmd);
 	pthread_mutex_destroy(&rpmut);
 }
 
